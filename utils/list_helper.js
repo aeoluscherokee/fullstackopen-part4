c3a4d101ef859a9e645dac635ba5b4247854de5c
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-10 14:36:08 +0700

    Add totalLikes to list_helper

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -4,6 +4,21 @@ const dummy = (blogs) => {
   }
 };
 
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  } else if (blogs.length === 1) {
+    return blogs[0].likes;
+  } else if (blogs.length >= 1) {
+    const sum = blogs.reduce(
+      (prevLikes, currentValue) => prevLikes + currentValue.likes,
+      0
+    );
+    return sum;
+  }
+};
+
 module.exports = {
   dummy,
+  totalLikes,
 };
