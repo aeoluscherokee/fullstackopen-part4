877fbd7983ad2bd94fc27353d40f5c0f4c689d0f
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-10 13:22:52 +0700

    Remove PORT constrant

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const config = require('./utils/config');
 
 const server = http.createServer(app);
 
-const PORT = config.PORT;
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });
