3aef3d0d14337a1fc79127a219c657aeae6d6c66
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-10 13:25:32 +0700

    Use blogs controller in app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const config = require('./utils/config');
-const Blog = require('./models/blog');
+const blogsRouter = require('./controllers/blogs');
 
 const app = express();
 
@@ -11,19 +11,6 @@ mongoose.connect(mongoUrl);
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
+app.use('/api/blogs', blogsRouter);
 
 module.exports = app;
