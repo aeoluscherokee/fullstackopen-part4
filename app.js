3b633f871c2eb474bc6ca0f376bbf57d63f0e979
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-11 21:12:24 +0700

    Add user controller to app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
 const middleware = require('./utils/middleware');
 
 const app = express();
@@ -13,6 +14,7 @@ mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
+app.use('/api/users', usersRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
