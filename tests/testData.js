c378be328dc06c217d2bf0194b3801adcc41937a
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-11 22:07:54 +0700

    Add test data for user api route

--- tests/testData.js
+++ tests/testData.js
@@ -83,6 +83,64 @@ const updateLikes = {
   likes: 3,
 };
 
+const initialUsers = [
+  {
+    _id: '618d36aae77920fef23dfa41',
+    username: 'abswan',
+    name: 'Alan Blackswan',
+    passwordHash:
+      '$2b$10$LQKlMsNhV2GC6j7TuKyJnO46Guh90LIKPr6C/S8rWUQPRrY.B8Pba',
+  },
+  {
+    _id: '618d36aae77920fef23dfa45',
+    username: 'cirrues',
+    name: 'Kenedy Suspero',
+    passwordHash:
+      '$2b$10$le1FQwJJLTOZUTUJDg4/x.WVk3A9A3dPhO9OO.5aT0f2JgxFDfdSy',
+  },
+  {
+    _id: '618d36abe77920fef23dfa49',
+    username: 'leeoencardia',
+    name: 'Cardine Anthony',
+    passwordHash:
+      '$2b$10$/3DC.RFfJu3VQVLI4O06lOEZC1vD9eFpWdHnGTTt.t17rj7/6olYS',
+  },
+];
+
+const newUser = {
+  username: 'leonacary',
+  name: 'Leon Ronaldes',
+  password: 'belikewhatyouareat20',
+};
+
+const newUserWithoutUsername = {
+  name: 'Richards',
+  password: 'iamolderthanyoumate',
+};
+
+const newUserWithoutPassword = {
+  username: 'richardiscoming',
+  name: 'Richards',
+};
+
+const newUserWithShortUsername = {
+  username: 'le',
+  name: 'Leon Ronaldes',
+  password: 'belikewhatyouareat20',
+};
+
+const newUserWithShortPassword = {
+  username: 'leonacary',
+  name: 'Leon Ronaldes',
+  password: 'be',
+};
+
+const newUserWithExistingUsername = {
+  username: 'leeoencardia',
+  name: 'Leon Ronaldes',
+  password: 'belikewhatyouareat20',
+};
+
 module.exports = {
   initialBlogs,
   newBlog,
@@ -91,4 +149,11 @@ module.exports = {
   newBlogWithoutUrl,
   newBlogWithoutBoth,
   updateLikes,
+  initialUsers,
+  newUser,
+  newUserWithoutUsername,
+  newUserWithoutPassword,
+  newUserWithShortUsername,
+  newUserWithShortPassword,
+  newUserWithExistingUsername,
 };
