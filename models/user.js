a2ceaae730c79f445faac8a1554c0db7fc6c8504
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-11 22:07:11 +0700

    Modify user schema

--- models/user.js
+++ models/user.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = new mongoose.Schema({
-  username: String,
+  username: { type: String, required: true, minLength: 3, unique: true },
   name: String,
-  passwordHash: String,
+  passwordHash: { type: String, required: true },
   blogs: [
     {
       type: mongoose.Schema.Types.ObjectId,
