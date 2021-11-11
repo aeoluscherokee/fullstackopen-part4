71db719f69cce868b899bc7950413334ceb2ff25
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-11 22:07:20 +0700

    Check password length before encrypt the password

--- controllers/users.js
+++ controllers/users.js
@@ -5,19 +5,25 @@ const User = require('../models/user');
 usersRouter.post('/', async (request, response, next) => {
   const body = request.body;
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+  if (!body.password || body.password.length < 3) {
+    response.status(400).json({
+      error: 'password must be atleast 3 characters long',
+    });
+  } else {
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash,
-  });
-  try {
-    const newUser = await user.save();
-    response.status(201).json(newUser);
-  } catch (error) {
-    next(error);
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+    });
+    try {
+      const newUser = await user.save();
+      response.status(201).json(newUser);
+    } catch (error) {
+      next(error);
+    }
   }
 });
 
