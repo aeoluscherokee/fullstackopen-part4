84f8d371f6779152dc5d939b2b01a318c707d74b
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-12 00:18:31 +0700

    Create user login POST route

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const usersRouter = require('express').Router();
 const User = require('../models/user');
+const config = require('../utils/config');
 
 usersRouter.get('/', async (request, response) => {
   const users = await User.find({}).populate('blogs');
@@ -31,6 +33,31 @@ usersRouter.post('/', async (request, response, next) => {
       next(error);
     }
   }
+});
+
+usersRouter.post('/login', async (request, response) => {
+  const body = request.body;
+  const user = await User.findOne({ username: body.username });
+  const isPasswordCorrect =
+    user === null
+      ? false
+      : await bcrypt.compare(body.password, user.passwordHash);
+
+  if (!(user && isPasswordCorrect)) {
+    return response.status(401).json({
+      error: 'invalid username or password',
+    });
+  }
+  const userForToken = {
+    username: user.username,
+    id: user._id,
+  };
+
+  const token = jwt.sign(userForToken, config.TOKEN_SECRET);
+
+  response
+    .status(200)
+    .send({ token, username: user.username, name: user.name });
 });
 
 module.exports = usersRouter;
