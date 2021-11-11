01d9587c3452eeaed17beb69d7e31135302749b8
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-11 18:59:41 +0700

    Use middleware in app.js

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const config = require('./utils/config');
 const blogsRouter = require('./controllers/blogs');
+const middleware = require('./utils/middleware');
 
 const app = express();
 
@@ -12,5 +13,7 @@ mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
 app.use('/api/blogs', blogsRouter);
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
 
 module.exports = app;
