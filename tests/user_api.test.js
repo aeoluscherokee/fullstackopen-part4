37667f24b13f246f020a4508da91ae5d4e28044c
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-12 15:18:03 +0700

    Modify users route test

--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -1,55 +1,69 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
-const User = require('../models/user');
-const testData = require('./testData');
+const helper = require('./test_helper');
 
 const app = require('../app');
 
 const api = supertest(app);
 
-beforeEach(async () => {
-  await User.deleteMany({});
-  let userObject1 = new User(testData.initialUsers[0]);
-  await userObject1.save();
-  let userObject2 = new User(testData.initialUsers[1]);
-  await userObject2.save();
-  let userObject3 = new User(testData.initialUsers[2]);
-  await userObject3.save();
+beforeAll(async () => {
+  await helper.createInitialUsers();
+});
+
+describe('blogs GET method', () => {
+  test('return the correct amount of users in the JSON format', async () => {
+    const response = await api.get('/api/users').send(helper.newUser);
+    expect(response.type).toBe('application/json');
+    expect(response.body).toHaveLength(helper.initialUsers.length);
+  });
+  test('return the users with populated blogs', async () => {
+    const userAuth = await api.post('/api/users/login').send({
+      username: helper.initialUsers[0].username,
+      password: helper.initialUsers[0].password,
+    });
+    await api
+      .post('/api/blogs')
+      .set('Authorization', `Bearer ${userAuth.body.token}`)
+      .send(helper.newBlog);
+    const response = await api.get('/api/users').send(helper.newUser);
+    expect(response.body[0].blogs.length).toBe(1);
+    expect(response.body[0].blogs[0].user).toBe('618d36aae77920fef23dfa41');
+  });
 });
 
 describe('blogs POST method', () => {
   test('return code 201 when new user succesfully created', async () => {
-    const response = await api.post('/api/users').send(testData.newUser);
+    const response = await api.post('/api/users').send(helper.newUser);
     expect(response.statusCode).toBe(201);
   });
   test('return code 400 when empty username', async () => {
     const response = await api
       .post('/api/users')
-      .send(testData.newUserWithoutUsername);
+      .send(helper.newUserWithoutUsername);
     expect(response.statusCode).toBe(400);
   });
   test('return code 400 when empty password', async () => {
     const response = await api
       .post('/api/users')
-      .send(testData.newUserWithoutPassword);
+      .send(helper.newUserWithoutPassword);
     expect(response.statusCode).toBe(400);
   });
   test('return code 400 when username is less than 3', async () => {
     const response = await api
       .post('/api/users')
-      .send(testData.newUserWithShortUsername);
+      .send(helper.newUserWithShortUsername);
     expect(response.statusCode).toBe(400);
   });
   test('return code 400 when password is less than 3', async () => {
     const response = await api
       .post('/api/users')
-      .send(testData.newUserWithShortPassword);
+      .send(helper.newUserWithShortPassword);
     expect(response.statusCode).toBe(400);
   });
   test('return code 400 when username is existing', async () => {
     const response = await api
       .post('/api/users')
-      .send(testData.newUserWithShortPassword);
+      .send(helper.newUserWithShortPassword);
     expect(response.statusCode).toBe(400);
   });
 });
