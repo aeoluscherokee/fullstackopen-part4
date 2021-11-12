ea348191e78753366715f4701de13ac98c144e19
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-12 11:40:30 +0700

    Use userExtractor middleware in specific route

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ mongoose.connect(mongoUrl);
 app.use(cors());
 app.use(express.json());
 app.use(middleware.tokenExtractor);
-app.use('/api/blogs', blogsRouter);
+app.use('/api/blogs', middleware.userExtractor, blogsRouter);
 app.use('/api/users', usersRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
