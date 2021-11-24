547f6f41570431aee83a45053421060abcf67607
Krittanu Royapan <krittanu.riyapan@gmail.com>
2021-11-25 01:26:51 +0700

    Create comment schema

---
+++ models/comment.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
+
+const commentSchema = new mongoose.Schema({
+  comment: { type: String, required: true },
+  blog: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Blog',
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+commentSchema.plugin(uniqueValidator);
+
+commentSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model('Comment', commentSchema);
